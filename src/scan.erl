a7dfc9d15374e1f26b368a086fe7150136b5f62f
Konstantin Voschanov <kavoschanov@platbox.com>
2014-11-14 12:59:05 +0400

    clean up code

--- src/scan.erl
+++ src/scan.erl
@@ -26,27 +26,28 @@ get_docs() ->
 
 make_index(DocTable, IndexTable) ->
     create_ets(IndexTable, [bag, named_table]),
-    [make_index(IndexTable, DocTable, N, ets:first(DocTable)) || N <- lists:seq(1,10) ],
+    [make_index(IndexTable, DocTable, N, ets:first(DocTable)) || N <- lists:seq(1,10)],
     lager:info("~p indexes created OK", [ets:info(IndexTable, size)]),
     ok.
 
 make_index(IndexTable, _DocTable, Size, '$end_of_table') ->    
     lager:info("~p (~p) indexes created OK", [Size, ets:info(IndexTable, size)]),
     ok;
-
 make_index(IndexTable, DocTable, Size, Key) ->
-    [_Module, MFun] = binary:split(Key, <<":">>),
+    {_Module, Fun, _Arity} = split_mfa(Key),
+    SubKey = get_part(Fun, Size),
+%    io:format("~p ~p ~p = ~p~n", [_Module, Fun, Key, SubKey]),
+    ets:insert(IndexTable, {SubKey, Key}),
+    make_index(IndexTable, DocTable, Size, ets:next(DocTable, Key)).
+
+split_mfa(MFA) ->
+    [Module, MFun] = binary:split(MFA, <<":">>),
     FunAr = case binary:split(MFun, <<":">>) of
         [F]    -> F;
         [_, F] -> F
     end,
-    [Fun, _Arity] = binary:split(FunAr, <<"/">>),
-    SubKey = get_part(Fun, Size),
-%    io:format("~p ~p ~p = ~p~n", [Module, Key, SubKey, FunAr]),
-%    lager:debug("~p ~p ~p = ~p~n", [Module, Key, SubKey, FunAr]),
-    ets:insert(IndexTable, {SubKey, Key}),
-    make_index(IndexTable, DocTable, Size, ets:next(DocTable, Key)).
-
+    [Fun, Arity] = binary:split(FunAr, <<"/">>),
+    {Module, Fun, Arity}.
 
 get_part(Bin, Size) ->
     case Size >= size(Bin) of
@@ -55,46 +56,40 @@ get_part(Bin, Size) ->
     end.
 
 scan_docs(Table) ->
-    lager:info("start grabbing erlang docs"),
     inets:start(),
+    lager:info("start grabbing erlang docs"),
     {ok, {{_Proto, 200, "OK"}, _Headers, Body}} = httpc:request("http://www.erlang.org/doc/man/array.html"),
     {_String, _Attributes, All} = mochiweb_html:parse(Body),
-    {_, Bd} = get_value(<<"body">>, All),
-    {_, R2} = get_value_by_attr(<<"div">>, <<"id">>, <<"container">>, Bd),
-    {_, R3} = get_value_by_attr(<<"div">>, <<"id">>, <<"leftnav">>, R2),
-    {_, R4} = get_value_by_attr(<<"div">>, <<"class">>, <<"innertube">>, R3),
-    {_, R5} = get_value_by_attr(<<"ul">>, <<"class">>, <<"flipMenu">>, R4),
+    {_, R1} = get_node(<<"body">>, All),
+    {_, R2} = get_node(<<"div">>, <<"id">>, <<"container">>, R1),
+    {_, R3} = get_node(<<"div">>, <<"id">>, <<"leftnav">>, R2),
+    {_, R4} = get_node(<<"div">>, <<"class">>, <<"innertube">>, R3),
+    {_, R5} = get_node(<<"ul">>,  <<"class">>, <<"flipMenu">>, R4),
     [ets:insert(Table, get_section(S)) || S <- get_top(R5)],
     lager:info("~p functions loaded OK", [ets:info(Table, size)]),
     ok.
 
 
-get_value(_Key, []) ->
-    undefined;
-get_value(Key, [Curr | Rest]) ->
-    case Curr of
-        {Key, Attr, Inner} -> {Attr, Inner};
-        _ -> get_value(Key, Rest)
-    end.
+get_node(_Tag, []) -> undefined;
+get_node(Tag, [{Tag, Attr, Inner} | _Rest]) -> {Attr, Inner};
+get_node(Tag, [_Curr | Rest]) -> get_node(Tag, Rest).
     
-
-get_value_by_attr(_Key, _AttrName, _AttrValue, []) ->
+get_node(_Tag, _AttrName, _AttrValue, []) ->
     undefined;
-get_value_by_attr(Key, AttrName, AttrValue, [Curr | Rest]) ->
-    case Curr of
-        {Key, Attr, Inner} -> 
-            case proplists:get_value(AttrName, Attr) of
-                AttrValue -> {Attr, Inner};
-                _ -> get_value(Key, Rest)
-            end;
-        _ -> get_value(Key, Rest)
-    end.
+get_node(Tag, AttrName, AttrValue, [{Tag, Attr, Inner} | Rest]) ->
+    case proplists:get_value(AttrName, Attr) of
+        AttrValue -> {Attr, Inner};
+        _ -> get_node(Tag, Rest)
+    end;
+get_node(Tag, _AttrName, _AttrValue, [_Curr | Rest]) ->
+    get_node(Tag, Rest).
 
 
 get_top(Tree) -> get_top([], Tree).
 
-get_top(Acc, []) 
-    -> lists:reverse(Acc);
+get_top(Acc, []) ->
+    lager:info("~p section loaded OK", [length(Acc)]),
+    lists:reverse(Acc);
 get_top(Acc, [{<<"li">>, Attr, Inner} | Rest]) ->
     case proplists:is_defined(<<"expanded">>, Attr) of
         true  -> get_top([{proplists:get_value(<<"title">>, Attr), Inner} | Acc], Rest);
@@ -104,13 +99,14 @@ get_top(Acc, [_Curr | Rest]) ->
     get_top(Acc, Rest).
 
 
-get_section({Name, [_, {<<"ul">>, _, Tree}]}) -> get_section(binary:replace(Name, <<" ">>, <<>>), [], Tree).
+get_section({SectionName, [_, {<<"ul">>, _, Tree}]}) -> 
+    get_section(binary:replace(SectionName, <<" ">>, <<>>), [], Tree).
 
 parse_ref(SectionName, [{<<"a">>, [{<<"href">>, Ref}], [Name]}]) -> 
     {<<SectionName/binary, ":", Name/binary>>, Ref}.
 
-get_section(_Name, Acc, [])
-    -> lists:reverse(Acc);
+get_section(_Name, Acc, []) -> 
+    lists:reverse(Acc);
 get_section(Name, Acc, [{<<"li">>, Attr, Inner} | Rest]) ->
     case proplists:is_defined(<<"title">>, Attr) of
         true  -> get_section(Name, [parse_ref(Name, Inner) | Acc], Rest);
